1222a8a446d8c9685b1237489abef7d80387f9ea
Otis <otisvickersgraver@gmail.com>
2020-11-06 16:27:16 +0000

    playing around with jquery

--- jqueryPlayground.js
+++ jqueryPlayground.js
@@ -1,7 +1,18 @@
 $(document).ready(function () {
-  $("input").click(function (event) {
-    alert("It's getting hot in here!");
-    event.preventDefault();
-  });
+  //
+  // $('input').click(function (event) {
+  //   alert('It\'s getting hot in here!');
+  //   event.preventDefault();
+  // });
+  //
+  // $('input').addClass('tempUp');
 
+  // $('input').removeClass('tempUp');
+  $( 'input' ).click(function( event ) {
+    console.dir(event);
+    event.preventDefault();
+
+    $( this ).hide( 'slow' );
+
+  });'slow';
 });
