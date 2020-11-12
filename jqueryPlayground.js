f37cca96bd5e19ba8018a4084e08b67857d9958a
Tim Castillo-Gill <timgillviola@gmail.com>
2020-11-12 12:35:45 +0000

    working API call from open weather

--- jqueryPlayground.js
+++ jqueryPlayground.js
@@ -1,24 +1,66 @@
 $(document).ready(function () {
 
-  $('input').addClass('tempUp');
 
-  $('input').removeClass('tempUp');
+  // $( 'input' ).click(function( event ) {
+  //   alert('It\'s getting hot in here!');
+  //   event.preventDefault();
+  //   $( this ).hide( 'slow' );
+  // });
 
-  $( 'input' ).click(function( event ) {
-    alert('It\'s getting hot in here!');
-    event.preventDefault();
-    $( this ).hide( 'slow' );
-  });
+  var thermostat = new Thermostat();
+  
+  function updateTemperature() {
+    $('.currentTempDisplay').text(thermostat.temperature);
+    // $('.currentTempDisplay').attr('class', thermostat.energyUsage());
+  }
+  updateTemperature();
 
-  function myCallBack() {
-    console.log("My call back function")
+  $('#up').on('click', function() {
+    thermostat.up();
+    updateTemperature();
+  })
+
+  $('#down').on('click', function() {
+    thermostat.down();
+    updateTemperature();
+  })
+
+  $('#reset').on('click', function() {
+    thermostat.reset();
+    updateTemperature();
+  })
+
+  $('#powerSavingMode').on('click', function() {
+    thermostat.turnOnPowerSavingMode();
+    $('#powerSavingMode').text('ON')
+    updateTemperature();
+  })
+
+  $('#powerSavingMode').on('click', function() {
+    thermostat.turnOffPowerSavingMode();
+    $('#powerSavingMode').text('OFF')
+    // alert('It\'s getting hot in here!');
+    updateTemperature();
+  })
+
+  function displayWeather(city) {
+    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city;
+    let token = '&appid=2e73c213db3e36f1e5a8e5b382f3b084';
+    let units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    })
   };
+    displayWeather('london');
 
-  $.get( "https://async-workshops-api.herokuapp.com/people",  (res) => {
-    console.log(res)
-  }, myCallBack());
+    $('#select-city').submit(function(event) {
+      event.preventDefault();
+      let city = $('#current-city').val();
+      displayWeather(city);
+    })
 
+    $('#reset').on('click', function() {
+      $('.energyUsage').slideToggle(200);
+    });
+})
 
-
-
-});
